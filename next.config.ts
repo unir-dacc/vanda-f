6cb7d26533127c791ad462140f0a1eab095fb070
Ricardo Alves <ricardcpu@gmail.com>
2026-04-29 21:36:45 -0400

    add basepath /vanda

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/vanda",
   eslint: {
     // ⚠️ Use só temporariamente
     ignoreDuringBuilds: true,
